390e4c45c33e34986cd218ba0061f1e1690912d0
mujay <muji.official06@gmail.com>
2025-04-23 13:09:22 +0700

    fix error build

--- src/app/(protected)/human-resource/absensi/page.tsx
+++ src/app/(protected)/human-resource/absensi/page.tsx
@@ -1,6 +1,17 @@
-export { default } from "./_components/absensi.page";
+import { Suspense } from "react";
+import AbsensiPage from "./_components/absensi.page";
 
 export const metadata = {
   description: "Absensi",
   title: "Absensi",
 };
+
+function page() {
+  return (
+    <Suspense>
+      <AbsensiPage />
+    </Suspense>
+  );
+}
+
+export default page;
