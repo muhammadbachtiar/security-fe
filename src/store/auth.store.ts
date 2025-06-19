8d17cbf6f786a38e840100d2f0fe84acb9535ffe
mujay <muji.official06@gmail.com>
2025-06-19 13:15:32 +0700

    store

---
+++ src/store/auth.store.ts
@@ -0,0 +1,60 @@
+import { PermissionType, UserType } from "@/services/auth/auth.dto";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+type UserStore = {
+  user: UserType | null;
+  coreToken: string | null;
+  hrdToken: string | null;
+  whToken: string | null;
+  hrdRole: PermissionType[];
+  coreRole: PermissionType[];
+  whRole: PermissionType[];
+  logout: () => void;
+  setUser: (value: UserType) => void;
+  setRole: (
+    type: "whRole" | "hrdRole" | "coreRole",
+    value: PermissionType[]
+  ) => void;
+  setToken: (type: "whToken" | "hrdToken" | "coreToken", value: string) => void;
+};
+
+export const useAuthStore = create<UserStore>()(
+  persist(
+    (set) => ({
+      user: null,
+      hrdToken: null,
+      whToken: null,
+      coreToken: null,
+      whRole: [],
+      hrdRole: [],
+      coreRole: [],
+      setRole: (type, value) =>
+        set((state) => ({
+          ...state,
+          [type]: value,
+        })),
+      setToken: (type, value) =>
+        set((state) => ({
+          ...state,
+          [type]: value,
+        })),
+      setUser: (value) =>
+        set((state) => ({
+          ...state,
+          user: value,
+        })),
+      logout: () =>
+        set({
+          user: null,
+          hrdToken: null,
+          whToken: null,
+          coreToken: null,
+          whRole: [],
+          hrdRole: [],
+          coreRole: [],
+        }),
+    }),
+    { name: "user_info" }
+  )
+);
